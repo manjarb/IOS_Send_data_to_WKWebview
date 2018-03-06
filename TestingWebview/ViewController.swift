2379429a72f7851511dec4a2a6993f3e80259224
Varis DARASIRKUL <varis@prenetics.com>
2018-03-06 11:41:05 +0800

    Initial Commit

---
+++ TestingWebview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingWebview
+//
+//  Created by Varis DARASIRKUL on 6/3/2018.
+//  Copyright © 2018 Varis DARASIRKUL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
